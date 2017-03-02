926790ef8ef6712ab30c6141cd5545b4fdbc05ed
xianengquan <1462927100@qq.com>
2017-03-02 11:13:55 +0800

    update

---
+++ onestong/ost-edge-crm/src/main/java/org/ost/crm/controller/project/ProjectTypeController.java
@@ -0,0 +1,18 @@
+package org.ost.crm.controller.project;
+
+import org.ost.crm.controller.base.Action;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="projectType")
+public class ProjectTypeController extends Action {
+	
+	public void  deleteProjectType(){
+
+	}
+	
+	
+	
+	
+}	
