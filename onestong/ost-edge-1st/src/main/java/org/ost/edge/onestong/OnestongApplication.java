b4f3e410bf60051a49de6767ce6c47b5249941f7
xianengquan <1462927100@qq.com>
2017-03-14 15:06:32 +0800

    update

--- onestong/ost-edge-1st/src/main/java/org/ost/edge/onestong/OnestongApplication.java
+++ onestong/ost-edge-1st/src/main/java/org/ost/edge/onestong/OnestongApplication.java
@@ -7,6 +7,7 @@ import org.ost.edge.onestong.inteceptor.CrossDomainInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -36,6 +37,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableSwagger2
+@EnableFeignClients
 public class OnestongApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
